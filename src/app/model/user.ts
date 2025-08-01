33aac878436e6adc5a2bef8753d013cc86906910
Mariana <marianaschmal@yahoo.de>
2025-08-01 14:36:37 +0200

    changed the user model. Added name and mobileNumber

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,16 +1,18 @@
 export class User{
   public id : number;
-  //public name : string;
+  public name : string;
   public email: string;
+  public mobileNumber: number;
   public password: string;
   public role: string;
   public authStatus: string;
 
 
-  constructor(id?: number, name?: string, email?: string, password?: string, role?: string, authStatus?: string){
+  constructor(id?: number, name?: string, email?: string, mobileNumber? : number , password?: string, role?: string, authStatus?: string){
     this.id =id || 0;
-  // this.name = name || "";
+    this.name = name || "";
     this.email = email || "";
+    this.mobileNumber = mobileNumber || 0;
     this.password = password || "";
     this.role = role || "";
     this. authStatus = authStatus || "";
