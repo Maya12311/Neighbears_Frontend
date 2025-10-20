ccaf46ccbcf29b493a009f1e2da8ff27f2e78c8a
Mariana <marianaschmal@yahoo.de>
2025-10-20 15:53:29 +0200

    need to implement the blob image. implementes service call. now furter do in backend

--- src/app/model/neighbear.ts
+++ src/app/model/neighbear.ts
@@ -1,10 +1,14 @@
 export class Neighbear
-{
+{public id: number;
   public name: string;
+  public avatar?: {
+    storageKey?: string | "";
+    contentType?: string | "";
+  }
 
-  constructor(name?: string) {
+  constructor(name?: string, id?: number) {
     this.name= name || "";
-
+    this.id = id || 0; 
   }
 
 }
